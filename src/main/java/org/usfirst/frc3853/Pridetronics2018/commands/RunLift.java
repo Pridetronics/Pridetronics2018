14f6eb740c8c73d7a88b3321b52a5893122f4e73
aluminumchassis2 <bciccarelli3853@gmail.com>
2019-01-31 16:26:58 -0700

    Fixed lift (STOP WITH YOUR RANDOM VARIABLES)

--- src/main/java/org/usfirst/frc3853/Pridetronics2018/commands/RunLift.java
+++ src/main/java/org/usfirst/frc3853/Pridetronics2018/commands/RunLift.java
@@ -22,7 +22,7 @@ public class RunLift extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	manageLift();
+    Robot.lift.up();
     }
    
 
